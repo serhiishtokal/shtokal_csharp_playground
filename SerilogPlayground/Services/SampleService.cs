07065f8f5d2a4b58a5912d619d0fb89e975a5a83
Serhii Shtokal <serhiishtokal@gmail.com>
2024-03-05 14:53:06 +0100

    test pushing properties to the ILogger<T> with Serilog

--- SerilogPlayground/Services/SampleService.cs
+++ SerilogPlayground/Services/SampleService.cs
@@ -17,13 +17,25 @@ public class SampleService
     {
         using (LogContext.PushProperty("SomeName", new {SomeProperty = "SomeValue"}))
         {
-            _serilogLogger.Warning("SERILOG SampleService.DoSomething called");
-            _serilogLogger.Warning("SERILOG SampleService.DoSomething called log 2");
+            _serilogLogger.Warning("SERILOG 1 SampleService.DoSomething called");
         }
         
-        using (_microsoftLogger.BeginScope(new { SomeProperty = "SomeValue" }))
+        using (_microsoftLogger.BeginScope(new Dictionary<string, object>
+               {
+                   ["SomeName"] = new {SomeProperty = "SomeValue"}
+               }))
         {
-            _microsoftLogger.LogWarning("Microsoft SampleService.DoSomething called");
+            _microsoftLogger.LogWarning("Microsoft 1 SampleService.DoSomething called");
         }
+        
+        // using (_microsoftLogger.BeginScope(new { SomeProperty = "SomeValue" }))
+        // {
+        //     using (_microsoftLogger.BeginScope(new { SomeProperty2 = "SomeValue" }) )
+        //     {
+        //         _microsoftLogger.LogWarning("Microsoft SampleService.DoSomething inner scope called"); 
+        //     }
+        //     
+        //     _microsoftLogger.LogWarning("Microsoft SampleService.DoSomething outer scope called");
+        // }
     }
 }
